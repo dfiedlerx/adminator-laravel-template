73ffbb8e248c072daec38e80d78272db61e56742
Izaías A <izaiasousa@gmail.com>
2018-11-30 08:18:38 -0300

    Traduções

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'E-mail ou senha inválidos.',
+    'throttle' => 'Detectamos demasiadas tentativas de acesso. Tente se conectar novamente em :seconds segundos.',
 
 ];
